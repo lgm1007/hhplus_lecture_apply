6a04a9cbe51d129b69cfb85db5fc105b66ecf9a6
lgm1007 <flxl654@gmail.com>
2024-09-28 19:11:13 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/com/example/hhpluslectureapply/HhplusLectureApplyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hhpluslectureapply
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusLectureApplyApplication
+
+fun main(args: Array<String>) {
+	runApplication<HhplusLectureApplyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hhpluslectureapply/HhplusLectureApplyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hhpluslectureapply
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusLectureApplyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
