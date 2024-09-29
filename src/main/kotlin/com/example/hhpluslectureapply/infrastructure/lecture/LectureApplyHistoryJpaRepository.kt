98ed4629f28ec14f2e2afd627a8bafae7a85a79b
lgm1007 <flxl654@gmail.com>
2024-09-29 20:57:29 +0900

    modify: lectureId에 해당하는 특강 신청완료 내역 Count 조회 메서드에 PESSIMISTIC_WRITE 락 설정

--- src/main/kotlin/com/example/hhpluslectureapply/infrastructure/lecture/LectureApplyHistoryJpaRepository.kt
+++ src/main/kotlin/com/example/hhpluslectureapply/infrastructure/lecture/LectureApplyHistoryJpaRepository.kt
@@ -2,12 +2,15 @@ package com.example.hhpluslectureapply.infrastructure.lecture
 
 import com.example.hhpluslectureapply.infrastructure.lecture.entity.LectureApplyHistory
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import javax.persistence.LockModeType
 
 interface LectureApplyHistoryJpaRepository : JpaRepository<LectureApplyHistory, Long> {
 	fun findByUserEmail(userEmail: String): LectureApplyHistory?
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT count(*) FROM LectureApplyHistory WHERE lectureId = :lectureId")
 	fun countByLectureId(@Param("lectureId") lectureId: Long): Long
 }
