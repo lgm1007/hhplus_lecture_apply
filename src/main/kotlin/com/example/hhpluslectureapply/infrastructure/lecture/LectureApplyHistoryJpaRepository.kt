d52fc7b8d8937dc50215e58800c6d12818f8cc82
lgm1007 <flxl654@gmail.com>
2024-09-29 20:56:36 +0900

    modify: lectureId에 해당하는 특강 신청완료 내역 목록 조회 JPQL 수정

--- src/main/kotlin/com/example/hhpluslectureapply/infrastructure/lecture/LectureApplyHistoryJpaRepository.kt
+++ src/main/kotlin/com/example/hhpluslectureapply/infrastructure/lecture/LectureApplyHistoryJpaRepository.kt
@@ -3,10 +3,11 @@ package com.example.hhpluslectureapply.infrastructure.lecture
 import com.example.hhpluslectureapply.infrastructure.lecture.entity.LectureApplyHistory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface LectureApplyHistoryJpaRepository : JpaRepository<LectureApplyHistory, Long> {
 	fun findByUserEmail(userEmail: String): LectureApplyHistory?
 
-	@Query("SELECT count(*)")
-	fun countByLectureId(lectureId: Long): Long
+	@Query("SELECT count(*) FROM LectureApplyHistory WHERE lectureId = :lectureId")
+	fun countByLectureId(@Param("lectureId") lectureId: Long): Long
 }
