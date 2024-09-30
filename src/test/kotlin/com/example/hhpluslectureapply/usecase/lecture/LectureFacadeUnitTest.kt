8cf0deac3ae506aa3b85f22e7cdbc06a46b62dfb
lgm1007 <flxl654@gmail.com>
2024-09-30 21:45:48 +0900

    test: 테스트 코드에서 Step3, 4에 해당하는 부분 걷어내기

--- src/test/kotlin/com/example/hhpluslectureapply/usecase/lecture/LectureFacadeUnitTest.kt
+++ src/test/kotlin/com/example/hhpluslectureapply/usecase/lecture/LectureFacadeUnitTest.kt
@@ -3,9 +3,7 @@ package com.example.hhpluslectureapply.usecase.lecture
 import com.example.hhpluslectureapply.domain.lecture.LectureApplyHistoryService
 import com.example.hhpluslectureapply.domain.lecture.LectureDto
 import com.example.hhpluslectureapply.domain.lecture.LectureService
-import com.example.hhpluslectureapply.exception.LectureException
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
-import org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -29,43 +27,14 @@ class LectureFacadeUnitTest {
 	lateinit var lectureFacade: LectureFacade
 
 	@Test
-	@DisplayName("특강 신청 중 이미 동일한 유저 이메일에 대한 신청 내역이 존재해 실패하는 예외케이스 검사")
-	fun shouldApplyLectureFailIsExistUserEmail() {
-		`when`(lectureApplyHistoryService.isExistLectureApplyHistory(any(), any()))
-			.thenReturn(true)
-
-		assertThatThrownBy {
-			lectureFacade.applyLecture(LectureApplyInfo(1L, 1L))
-		}.isInstanceOf(LectureException::class.java)
-			.hasMessageContaining("유저가 이미 신청한 내역이 있습니다.")
-	}
-
-	@Test
-	@DisplayName("특강 신청 중 특강의 최대 정원이 초과되어 실패하는 예외케이스 검사")
-	fun shouldApplyLectureFailMaxNumberApply() {
-		val MAX_NUMBER_APPLY_LECTURE = 30
-
-		`when`(lectureApplyHistoryService.countLectureApplyHistoryInfosByLectureId(any()))
-			.thenReturn(MAX_NUMBER_APPLY_LECTURE)
-
-		assertThatThrownBy {
-			lectureFacade.applyLecture(LectureApplyInfo(1L, 1L))
-		}.isInstanceOf(LectureException::class.java)
-			.hasMessageContaining("특강은 최대 정원이 마감되었습니다.")
-	}
-
-	@Test
 	@DisplayName("현재 신청 가능한 특강 목록 조회하는 기능 단위 테스트 - 특강 정원이 마감되었는지 필터링 기능 검증")
 	fun getAllAppliableLectures() {
-		val MAX_NUMBER_APPLY_LECTURE = 30
 
 		`when`(lectureService.getAllLecturesByApplicationDateBefore(any()))
 			.thenReturn(givenLectures())
-		`when`(lectureApplyHistoryService.countLectureApplyHistoryInfosByLectureId(any()))
-			.thenReturn(MAX_NUMBER_APPLY_LECTURE)
 
 		val actual = lectureFacade.getAllAppliableLectures()
-		assertThat(actual.size).isEqualTo(0)
+		assertThat(actual.size).isEqualTo(2)
 	}
 
 	private fun givenLectures(): List<LectureDto> {
