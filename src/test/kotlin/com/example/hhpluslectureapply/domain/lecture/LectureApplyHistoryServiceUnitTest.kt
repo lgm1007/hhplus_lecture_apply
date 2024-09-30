59c81493fd82f2707e52bbfe8295c8ae477f47f4
lgm1007 <flxl654@gmail.com>
2024-09-30 22:34:27 +0900

    test: 현재 신청 가능한 특강 목록 조회 시 신청 인원 마감되었는지 검증하는 부분 테스트 코드 추가

---
+++ src/test/kotlin/com/example/hhpluslectureapply/domain/lecture/LectureApplyHistoryServiceUnitTest.kt
@@ -0,0 +1,37 @@
+package com.example.hhpluslectureapply.domain.lecture
+
+import org.assertj.core.api.AssertionsForClassTypes
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class LectureApplyHistoryServiceUnitTest {
+	@Mock
+	lateinit var lectureApplyHistoryRepository: LectureApplyHistoryRepository
+
+	@InjectMocks
+	lateinit var lectureApplyHistoryService: LectureApplyHistoryService
+
+	@Test
+	@DisplayName("특정한 특강의 인원수가 아직 마감되지 않았는지 체크하는 테스트")
+	fun isNotFullCountLectureMaxApply() {
+		`when`(lectureApplyHistoryRepository.countByLectureId(1L))
+			.thenReturn(20)
+		`when`(lectureApplyHistoryRepository.countByLectureId(2L))
+			.thenReturn(30)
+
+		val actual1 = lectureApplyHistoryService.isNotFullCountLectureMaxApply(1L)
+		val actual2 = lectureApplyHistoryService.isNotFullCountLectureMaxApply(2L)
+
+		assertAll(
+			{ AssertionsForClassTypes.assertThat(actual1).isTrue() },
+			{ AssertionsForClassTypes.assertThat(actual2).isFalse() }
+		)
+	}
+}
