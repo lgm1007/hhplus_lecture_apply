c84691c18b5b916daad1ee0d4082715afd978a1c
lgm1007 <flxl654@gmail.com>
2024-09-30 21:34:48 +0900

    test: 현재 신청 가능한 특강 목록 조회하는 기능 단위 테스트 - 특강 정원이 마감되었는지 필터링 기능 검증 테스트코드 추가

--- src/test/kotlin/com/example/hhpluslectureapply/usecase/lecture/LectureFacadeUnitTest.kt
+++ src/test/kotlin/com/example/hhpluslectureapply/usecase/lecture/LectureFacadeUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.hhpluslectureapply.usecase.lecture
 
 import com.example.hhpluslectureapply.domain.lecture.LectureApplyHistoryService
+import com.example.hhpluslectureapply.domain.lecture.LectureDto
 import com.example.hhpluslectureapply.domain.lecture.LectureService
 import com.example.hhpluslectureapply.exception.LectureException
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
@@ -13,6 +15,7 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
+import java.time.LocalDateTime
 
 @ExtendWith(MockitoExtension::class)
 class LectureFacadeUnitTest {
@@ -49,6 +52,27 @@ class LectureFacadeUnitTest {
 			lectureFacade.applyLecture(LectureApplyInfo(1L, 1L))
 		}.isInstanceOf(LectureException::class.java)
 			.hasMessageContaining("특강은 최대 정원이 마감되었습니다.")
+	}
+
+	@Test
+	@DisplayName("현재 신청 가능한 특강 목록 조회하는 기능 단위 테스트 - 특강 정원이 마감되었는지 필터링 기능 검증")
+	fun getAllAppliableLectures() {
+		val MAX_NUMBER_APPLY_LECTURE = 30
+
+		`when`(lectureService.getAllLecturesByApplicationDateBefore(any()))
+			.thenReturn(givenLectures())
+		`when`(lectureApplyHistoryService.countLectureApplyHistoryInfosByLectureId(any()))
+			.thenReturn(MAX_NUMBER_APPLY_LECTURE)
+
+		val actual = lectureFacade.getAllAppliableLectures()
+		assertThat(actual.size).isEqualTo(0)
+	}
+
+	private fun givenLectures(): List<LectureDto> {
+		return listOf<LectureDto>(
+			LectureDto(1L, "title1", "강사1", LocalDateTime.now()),
+			LectureDto(2L, "title2", "강사2", LocalDateTime.now())
+		)
 	}
 
 }
